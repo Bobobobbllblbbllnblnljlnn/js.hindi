4794ff4e1f0376268bfd274958ae08294f8f68cf
Rajeevmahera <163811007+Bobobobbllblbbllnblnljlnn@users.noreply.github.com>
2024-08-30 10:30:16 +0000

    container config done

---
+++ 01_Basic/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 13242;
+let accountEmail = "raj123@gmail.com";
+var accountPassword = "12345";
+accountCity = "noida";
+let accountState;
+
+// accountId = 787878; not allowed 
+
+accountEmail = "hchc@1234.gamil.com";
+accountPassword = "212121";
+accountPassword = "1212121"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail , accountPassword , accountCity , accountState]);
