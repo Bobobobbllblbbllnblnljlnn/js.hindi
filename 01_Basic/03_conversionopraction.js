eec894f11206d581dcd3e77a325c18a579242bb3
Rajeevmahera <163811007+Bobobobbllblbbllnblnljlnn@users.noreply.github.com>
2024-08-31 08:18:35 +0000

    Conversion of Datatype

---
+++ 01_Basic/03_conversionopraction.js
@@ -0,0 +1,28 @@
+ let score = undefined;
+
+// console.log(typeof(score));
+// console.log(typeof score);
+
+  let valueInNUmber = Number(score);
+// console.log(typeof valueInNUmber);
+
+// console.log(valueInNUmber);
+
+// 33 => 33
+// "33abc" => NaN
+// true 1 / false 0
+
+let IsLoggedIn = "rjeev";
+
+let  booleanIsLoggedIn = Boolean(IsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => 0 / false => 2;
+// "" => false 
+// rajeev => true
+
+let sumNUmber = 33;
+
+let stringNumber = String(sumNUmber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
