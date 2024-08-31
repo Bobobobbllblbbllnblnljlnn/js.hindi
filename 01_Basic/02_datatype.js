c447a9befd42cd06c9f9c594c827cc6b3d7f5dbf
Rajeevmahera <163811007+Bobobobbllblbbllnblnljlnn@users.noreply.github.com>
2024-08-31 07:32:39 +0000

    datatype

---
+++ 01_Basic/02_datatype.js
@@ -0,0 +1,28 @@
+"use strict" // treat js code as newer version 
+
+// alert( 3 + 3 ); we are using nodejs not browser;
+
+console.log(3
+    
+    +3);  // code readability should be high;
+
+console.log("rajeev");
+
+// type's of data type 
+
+let name  = "rajeev";
+let age = 21;
+let loginId = false;
+
+// number => 2 to power 53
+//bigint 
+// string => "" ''
+//boolean 
+// null  standalone value 
+// undefind => 
+// symobl => unique
+
+// object
+
+console.log(typeof null); //object
+console.log(typeof undefined); // undefind
