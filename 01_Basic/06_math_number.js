71da924c46913f8cfa9f75c31bb677f877247e6a
Rajeevmahera <163811007+Bobobobbllblbbllnblnljlnn@users.noreply.github.com>
2024-09-06 18:22:27 +0000

    Math_number

---
+++ 01_Basic/06_math_number.js
@@ -0,0 +1,37 @@
+const score = 100;
+//console.log(score);
+
+const balnse =  new Number(200);
+//console.log(balnse); 
+
+//console.log(balnse.toString().length);
+//console.log(balnse.toFixed(2));
+
+const otherNumber = 312.6;
+//console.log(otherNumber.toPrecision(3));
+
+const hundersd = 1000000;
+//console.log(hundersd.toLocaleString(`en-IN`));
+
+// *************Math************
+
+// console.log(Math.abs(-55));
+// console.log(Math.round(1.6));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.3));
+// console.log(Math.sqrt(36));
+// console.log(Math.fround(25));
+// console.log(Math.min(5,8,5,6,56,55));
+// console.log(Math.max(5,8,5,6,56,55));
+
+// most use to math laberay random
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min +1)))+min;
+
+                                                                                                                                  
