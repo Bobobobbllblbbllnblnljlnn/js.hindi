e3d4ad170b7c735722f601bf051f9a82d764cbd9
Rajeevmahera <163811007+Bobobobbllblbbllnblnljlnn@users.noreply.github.com>
2024-09-07 10:08:51 +0000

    Array method

---
+++ 02_Basic/01_array.js
@@ -0,0 +1,42 @@
+
+const myArr = [0, 1, 2, 3, 4, 5,];
+//  console.log(myArr[1]);
+ 
+
+ const myHero = ["rajeev", "mumtaj"];
+ const Arr = [ 1,5,58,5,6,8,6,85,98]; 
+//  console.log(myHero[1]);
+//  console.log(Arr[4]);
+
+ // Array methods
+
+//  myArr.push(6);
+//  myArr.push(7);
+//  myArr.pop();
+
+// myArr.unshift(9);
+// //  myArr.shift();
+
+// console.log(myArr.includes(5));
+// console.log(myArr.indexOf(5));
+
+// const newArr = myArr.join();
+// console.log(newArr);
+
+// console.log(myArr);
+
+// slice , splice
+
+// console.log("A " , myArr);
+
+// const myn1 = myArr.slice(1 ,3);
+
+// console.log(myn1);
+// console.log("B " , myArr);
+
+console.log("A " , myArr);
+const myn2 = myArr.splice(1 ,3);
+console.log("c " , myArr);
+console.log(myn2);
+
+
