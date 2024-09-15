cf8704bd935bed367f03a958e57ca7564342b66b
Rajeevmahera <163811007+Bobobobbllblbbllnblnljlnn@users.noreply.github.com>
2024-09-15 14:05:43 +0000

    globle and block scope

---
+++ 03_Basice/01_scope.js
@@ -0,0 +1,15 @@
+// var  c = 300;
+
+let b = 300;
+
+if (true) {
+  const a = 10;
+  let b = 20;
+//   var c = 30;
+console.log("inner:", a);
+}
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
