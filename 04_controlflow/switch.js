84adb90965ecaf52f4b660dc8e43b3bd3342908d
Rajeevmahera <163811007+Bobobobbllblbbllnblnljlnn@users.noreply.github.com>
2024-09-20 17:59:48 +0000

    switch statement

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,39 @@
+// const month = 3;
+
+// switch(month){
+//     case 1:
+//     console.log("Junery");
+//     break;
+//     case 2:
+//     console.log("Feveury");
+//     break;
+//     case 3:
+//     console.log("March");
+//     break;
+//     case 4:
+//     console.log("april");
+//     break;
+
+//     default:
+//         console.log("defoult case");
+// }
+
+
+const month = "march";
+switch(month){
+    case "jan":
+    console.log("Junery");
+    break;
+    case "fev":
+    console.log("Feveury");
+    break;
+    case "march":
+    console.log("March");
+    break;
+    case "april":
+    console.log("april");
+    break;
+
+    default:
+        console.log("defoult case");
+}
