b27c89915618cf39eac59fcbc607f22d8084d109
Rajeevmahera <163811007+Bobobobbllblbbllnblnljlnn@users.noreply.github.com>
2024-09-20 17:57:30 +0000

    controlflow_statement

---
+++ 04_controlflow/one.js
@@ -0,0 +1,59 @@
+// control flow: statement; / logic controlflow
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+const isloggiden = true;
+
+if(2 !== 3){
+    console.log("exectued");
+}
+
+   const tempracher = 41;
+// if( tempracher === 40){
+//     console.log("less then 50")
+// }else{
+//     console.log("temracher is gratter then 50")
+// }
+
+const score = 200;
+
+// if(score > 100){
+//     const power = "fly";
+//     console.log(`user power ${power}`);
+// }
+
+// console.log(`user power ${power}`);
+
+//  shorethend notesaction ;
+
+const balance = 1000;
+// if(balance > 500 )console.log("test"),console.log("test2"); naveruse 
+
+// nestig if elsee
+
+// if(balance < 500){
+//     console.log("less then");
+// }else if(balance < 750){
+//     console.log("less then 750");
+// }else if(balance < 900){
+//     console.log("less then 900");
+// }else{
+//     console.log("less then 1200")
+// }
+
+// and(&&) ouroperator (||)
+
+const uerlogdine = true
+const devitCard = true
+const loggedeinGoogle = false
+const loggedinformEmail = true
+
+if(uerlogdine && devitCard && 2 == 2 ){
+    console.log("Allowed to bey course ");
+}
+
+if (loggedeinGoogle || loggedinformEmail) {
+    console.log("user logdin");
+}
+
+
