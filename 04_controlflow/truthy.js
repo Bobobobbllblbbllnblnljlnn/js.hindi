b0e7e35b0d1c5cb5f406b5388e02267ccd18dbb6
Rajeevmahera <163811007+Bobobobbllblbbllnblnljlnn@users.noreply.github.com>
2024-09-20 18:02:23 +0000

    Truthy and falsy value

---
+++ 04_controlflow/truthy.js
@@ -0,0 +1,53 @@
+// truthy ans falssy value ;
+
+const userEmail = "kashyaishgh1u@gmail.com";
+
+// if(userEmail){
+//     console.log("Got the user Email");
+// }else{
+//     console.log("dont' have user:");
+// }
+
+// truthy value 
+// "0" , 'in any value truthy' " sem" , {} , funcation(){}
+
+//valsy value
+
+//false, 0, -0 , bigint 0n, "", null, undefined, NaN 
+
+const userName = "";
+// if(userName.length === 0){
+//     console.log("array is empty");
+// }
+
+const emptyobject = {}
+
+// if(Object.keys(emptyobject).length === 0){
+//     console.log("object is empty")
+// }
+ 
+//Nulish Coalescing operator (??) null undefined
+
+// let val1;
+// val1 = 5 ?? 10;
+// console.log(val1);
+
+// let val2;
+// val2 = null ?? 10
+// console.log(val2);
+
+// let val3;
+// val3 = undefined ?? 15;
+// console.log(val3);
+
+// val4 = null ?? 10 ?? 20;
+// console.log(val4);
+
+// Terniary Operator
+
+//condition ? true : false
+
+const iceTeaPrice = 100;
+
+iceTeaPrice <= 80 ? console.log("less then 80"): console.log("more then 80"); 
+
