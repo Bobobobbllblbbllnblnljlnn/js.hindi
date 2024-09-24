5c3d646f37e21d6daa60db9fc30bcef93ed97205
Rajeevmahera <163811007+Bobobobbllblbbllnblnljlnn@users.noreply.github.com>
2024-09-24 13:26:56 +0000

    for in loop

---
+++ 05_loops/loop_part4.js
@@ -0,0 +1,18 @@
+// for in loop
+
+const myobject = {
+    js: 'javascripe',
+    cpp: 'c++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+
+for (const key in myobject) {
+ // console.log(`${key} :- shortcut is for ${myobject[key]}`);
+        
+    }
+
+    const programming = ["js", "rb", "hy", "java" , "cop"];
+    for (const key in programming) {
+    console.log(` ${programming[key]}`)
+    }
