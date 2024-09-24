6f58ae6641417325cbdc1cf6d405aa076efb07c1
Rajeevmahera <163811007+Bobobobbllblbbllnblnljlnn@users.noreply.github.com>
2024-09-24 13:20:57 +0000

    while loop & do while loop

---
+++ 05_loops/loop_part2.js
@@ -0,0 +1,23 @@
+//while loop 
+
+let index = 0;
+while (index <= 10) {
+    //console.log(`value of index is ${index}`);
+    index = index + 2;
+}
+
+let myArrar = ["rajev" , "vishal" , "sandeep", "rakesh", "mumtaj" ,"viash"];
+let arf = 0;
+while (arf < myArrar.length) {
+   // console.log(`value is ${myArrar[arf]}`);
+    arf = arf + 2;
+}
+
+// do whil loop
+
+let score = 11;
+do{
+    console.log(`score is ${score}`);
+    score++
+}while(score <= 10);
+
